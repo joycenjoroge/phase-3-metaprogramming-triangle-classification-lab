a549d63c214b4edeedd01b18f1bdb71f85ae01b3
Joyce <joycenjoroge47@gmail.com>
2023-03-29 12:20:32 +0300

    Codility

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
   # write code here
+  attr_reader :length1,  :length2, :length3
+
+  def initialize(length1,length2,length3)
+    @length1=length1
+    @length2=length2
+    @length3=length3
+  end
+
+  def kind
+    valid_triangle
+    if length1==length2 && length1==length3
+      :equilateral
+    elsif length1==length2 || length1==length3 || length2==length3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+  
+  def valid_triangle
+    if length1 <= 0 || length2<= 0|| length3<= 0
+      raise TriangleError
+    elsif length1+length2<=length3 || length1+length3<=length2 || length2+length3<=length1
+      raise TriangleError
+    end
+  end
 end
